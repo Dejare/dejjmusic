d5912f01a78fba55ab00edf29d198ca2a7eba55a
Adejare <inioluwaadesida@gmail.com>
2022-06-18 14:57:09 +0100

    fixing mobile

--- src/component/dashboard/Dashboard.js
+++ src/component/dashboard/Dashboard.js
@@ -1,17 +1,39 @@
-import React from 'react'
-import Center from './Center'
-import Left from './Left'
-import Right from './Right'
+import React, { useState, useEffect } from "react";
+import Center from "./Center";
+import Left from "./Left";
+import Right from "./Right";
 
 const Dashboard = () => {
+    const [Mobile, setMobile] = useState(false);
+
+    useEffect(() => {
+        if (window.innerWidth < 768) {
+            setMobile(true);
+        } else if (window.innerWidth < 1024) {
+            setMobile(false);
+        } else {
+            setMobile(false);
+        }
+
+    }, []);
 
     return (
-        <div className='flex flex-col md:flex-row md:max-w-screen'>
-            <Left />
-            <Center />
-            <Right />
-        </div>
-    )
-}
+        <>
+            {Mobile ? (
+                <div className="flex flex-col md:flex-row md:max-w-screen">
+                    <Right />
+                    <Left />
+                    <Center />
+                </div>
+            ) : (
+                <div className="flex flex-col md:flex-row md:max-w-screen">
+                    <Left />
+                    <Center />
+                    <Right />
+                </div>
+            )}
+        </>
+    );
+};
 
-export default Dashboard
+export default Dashboard;
--- src/component/dashboard/Left.js
+++ src/component/dashboard/Left.js
@@ -19,7 +19,7 @@ const Left = () => {
     const handleSearch = (e) => { e.preventDefault() }
     // 
     return (
-        <div className="left  min-h-screen">
+        <div className="left h-full md:min-h-screen">
             <header className="flex flex-row items-center py-2 mt-8 text-white text-2xl font-normal justify-center">
                 Adejarree
             </header>
@@ -37,60 +37,62 @@ const Left = () => {
                 </form>
             </div>
             {/* MENUUU */}
-            <div className="menu text-gray-500 m-8 ml-10">
-                <h3 className="mb-4">MENU</h3>
-                <ul className="">
-                    <li className="flex flex-row items-center text-gray-300 mb-2">
-                        {" "}
-                        <RiHome5Fill className="text-white mr-2" /> Home
-                    </li>
-                    <li className="flex flex-row items-center mb-2">
-                        {" "}
-                        <RiCompassDiscoverFill className="mr-2" /> Discover
-                    </li>
-                    <li className="flex flex-row items-center mb-2">
-                        {" "}
-                        <RiRadio2Fill className="mr-2" /> Radio
-                    </li>
-                    <li className="flex flex-row items-center mb-2">
-                        {" "}
-                        <SiApplepodcasts className="mr-2" />
-                        Podcast
-                    </li>
-                </ul>
-            </div>
+            <div className="flex md:flex-col flex-row items-start justify-center">
+                <div className="menu text-gray-500 m-8 ml-10">
+                    <h3 className="mb-4">MENU</h3>
+                    <ul className="">
+                        <li className="flex flex-row items-center text-gray-300 mb-2">
+                            {" "}
+                            <RiHome5Fill className="text-white mr-2" /> Home
+                        </li>
+                        <li className="flex flex-row items-center mb-2">
+                            {" "}
+                            <RiCompassDiscoverFill className="mr-2" /> Discover
+                        </li>
+                        <li className="flex flex-row items-center mb-2">
+                            {" "}
+                            <RiRadio2Fill className="mr-2" /> Radio
+                        </li>
+                        <li className="flex flex-row items-center mb-2">
+                            {" "}
+                            <SiApplepodcasts className="mr-2" />
+                            Podcast
+                        </li>
+                    </ul>
+                </div>
 
-            {/* LIBRARY */}
+                {/* LIBRARY */}
 
-            <div className="menu text-gray-500 m-8 ml-10">
-                <h3 className="mb-4">LIBRARY</h3>
-                <ul className="">
-                    <li className="flex flex-row items-center text-gray-300 mb-2">
-                        {" "}
-                        <RiFolderOpenFill className="text-white mr-2" /> Album
-                    </li>
-                    <li className="flex flex-row items-center mb-2">
-                        {" "}
-                        <RiFolderMusicFill className="mr-2" /> Song
-                    </li>
-                    <li className="flex flex-row items-center mb-2">
-                        {" "}
-                        <RiUserSmileFill className="mr-2" />
-                        Artists
-                    </li>
-                </ul>
-            </div>
+                <div className="menu text-gray-500 m-8 ml-10">
+                    <h3 className="mb-4">LIBRARY</h3>
+                    <ul className="">
+                        <li className="flex flex-row items-center text-gray-300 mb-2">
+                            {" "}
+                            <RiFolderOpenFill className="text-white mr-2" /> Album
+                        </li>
+                        <li className="flex flex-row items-center mb-2">
+                            {" "}
+                            <RiFolderMusicFill className="mr-2" /> Song
+                        </li>
+                        <li className="flex flex-row items-center mb-2">
+                            {" "}
+                            <RiUserSmileFill className="mr-2" />
+                            Artists
+                        </li>
+                    </ul>
+                </div>
 
-            {/* Playlist */}
-            <div className="menu text-gray-500 m-8 ml-10">
-                <h3 className="mb-4">LIBRARY</h3>
-                <ul className="flex flex-row items-center justify-between">
-                    <li className="flex flex-row items-center text-gray-300 mb-2">
-                        {" "}
-                        <RiFolderOpenFill className="text-white mr-2" /> Emptyy
-                    </li>
-                    <RiAddFill className="text-white" />
-                </ul>
+                {/* Playlist */}
+                <div className="menu text-gray-500 m-8 ml-10">
+                    <h3 className="mb-4">LIBRARY</h3>
+                    <ul className="flex flex-row items-center justify-between">
+                        <li className="flex flex-row items-center text-gray-300 mb-2">
+                            {" "}
+                            <RiFolderOpenFill className="text-white mr-2" /> Emptyy
+                        </li>
+                        <RiAddFill className="text-white" />
+                    </ul>
+                </div>
             </div>
         </div>
     );
